977e264b8ea7a4d9c4defc00ea498ff5a1b9ef89
MiltonBlack <arziblack2@gmail.com>
2024-03-23 01:08:50 +0100

    updated to mongodb prod uri1...

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-// const MONGO_PROD_URI = "mongodb+srv://Blackdice:Black177@cluster0.1gxkqc6.mongodb.net/MCQ?retryWrites=true&w=majority&appName=Cluster0"
+const MONGO_PROD_URI = "mongodb+srv://Blackdice:Black177@cluster0.1gxkqc6.mongodb.net/MCQ?retryWrites=true&w=majority&appName=Cluster0"
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_PROD_URI as string);
+    await mongoose.connect(process.env.MONGO_PROD_URI as string || MONGO_PROD_URI);
   } catch (error) {
     console.log(error)
     process.exit(1)
